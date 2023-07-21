fe3a3bcd43a9f0a0972bee81350b9bef92b559bc
AngelFmc <angelfmc01@hotmail.com>
2023-07-21 15:19:28 -0600

    Create Update state
    Se hizo la función para actualizar el estado, creando la accion y accediendo al estado de almacenamiento

--- zustand-git/src/App.tsx
+++ zustand-git/src/App.tsx
@@ -2,7 +2,7 @@
 import { useBookStore } from "./store/bookStore"; //Se importa la store
 
 //Para difundir el objeto con multiples estados importamos shallow
-import { shallow } from "zustand/shallow";
+// import { shallow } from "zustand/shallow";
 
 function App() {
   //Se llama el hook, en el parametro se indica que propiedad se quiere extraer del store
@@ -13,19 +13,23 @@ function App() {
 
   // De esta manera se genera un objeto con multiples estados o propiedades
 
-  const { amount, title } = useBookStore(
-    (state) => ({
-      amount: state.amount,
-      title: state.author,
-    }),
-    shallow
-  );
+  // const { amount, title } = useBookStore(
+  //   (state) => ({
+  //     amount: state.amount,
+  //     title: state.author,
+  //   }),
+  //   shallow
+  // );
+
+  const amount = useBookStore((state) => state.amount);
+  const updareAmount = useBookStore((state) => state.updateAmount);
 
   return (
     <>
       <div>
         <h1>Books: {amount}</h1>
-        <h4>Title: {title}</h4>
+
+        <button onClick={() => updareAmount(10)}>Update Amount</button>
       </div>
     </>
   );
--- zustand-git/src/store/bookStore.ts
+++ zustand-git/src/store/bookStore.ts
@@ -5,11 +5,33 @@ import create from "zustand"; // Se importo la libreria
 // El interfaz define las propiedades del store
 interface IBook {
   amount: number;
-  author: string;
+
+  //Propiedad paraactualizar el amount (recibira el numero como parametro)
+  updateAmount: (newAmount: number) => void;
 }
 
 //Tetorna un objeto y create toma la funcion callback como parametro
-export const useBookStore = create<IBook>(() => ({
-  amount: 40, //Se establecio el valor inicial de amount (pertenece al interfaz)
-  author: "Este es un texto ",
+// export const useBookStore = create<IBook>(() => ({
+//   amount: 40, //Se establecio el valor inicial de amount (pertenece al interfaz)
+//   author: "Este es un texto ",
+// }));
+
+//Actualizando estado
+// export const useBookStore = create<IBook>((set) => ({
+//   amount: 40,
+//   updateAmount: (newAmount: number) => set({ amount: newAmount }),
+// }));
+
+//Accediendo al estado almacenado en la store
+export const useBookStore = create<IBook>((set, get) => ({
+  amount: 40,
+  updateAmount: (newAmout: number) => {
+    // el get nos da acceso al estado
+    const amountState = get().amount;
+
+    //el set definir el estado usamos set
+    set({
+      amount: newAmout + amountState,
+    });
+  },
 }));
